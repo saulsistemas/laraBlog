749ddddac0f02a64930eea3beb1f2c500c8368a5
Saul laravel <santamariaramos18@hotmail.com>
2021-12-14 10:22:14 -0500

    crear filtro por categoria

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -54,5 +55,14 @@ class PostController extends Controller
     public function destroy($id)
     {
         //
+    }
+    public function category(Category $category)
+    {
+        $posts = Post::where('category_id',$category->id)
+                ->where('status',2)
+                ->latest('id')
+                ->paginate(2);
+        #return $posts;
+        return view('post.category',compact('posts','category'));
     }
 }
